84a524f42d117dcc0c7cde02da8f6a98b62fc550
Julio César Martinez <juliocesarmartinez1344@gmail.com>
2026-09-02 22:58:59 -0500

    Revert "Add Docker deployment configuration"
    This reverts commit 1a0d6e2e58e71fb0e0733a913e0630d60a334d24.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	// Generates a minimal self-contained server image for Docker.
-	output: "standalone",
+  /* config options here */
 };
 
 export default nextConfig;
